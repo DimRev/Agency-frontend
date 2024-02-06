1183f5446474b80e22d9789d12fc99ac289e20c0
DimRev <dimarev444@gmail.com>
2024-02-06 18:22:31 +0200

    Create app home view

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Inside grid layout w.e. not a hero should get cols-start-2 -->
+  <Hero class="col-start-1 col-end-4" />
+  <section class="col-start-2">
+    <ReasonsSection />
+  </section>
+</template>
+
+<script setup>
+import Hero from '../components/Hero.vue'
+import ReasonsSection from '../components/ReasonsSection.vue'
+</script>
+<style ></style>
