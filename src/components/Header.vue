72ccb696a5d5d979495d292db1ac8bac0c02a1a9
DimRev <dimarev444@gmail.com>
2024-02-06 18:22:18 +0200

    Create app header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="flex justify-between py-6 border-b border-gray-12 px-[162px]">
+    <div>
+      <a href="/">
+        <LogoSvg />
+      </a>
+    </div>
+    <Navbar />
+  </header>
+</template>
+<script setup>
+import Navbar from './Navbar.vue'
+import LogoSvg from '../assets/svgs/LogoSvg.vue'
+
+</script>
+<style ></style>
