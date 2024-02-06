b337c72677d0d1e7cd982b2abe61086bf51b5a56
DimRev <dimarev444@gmail.com>
2024-02-06 18:23:00 +0200

    Add Header & Home to app, and set base

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid grid-cols-[162px_1fr_162px] grid-rows-[auto_1fr] min-h-[100dvh]">
+    <Header class="col-span-3" />
+    <Home />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Home from './views/Home.vue'
+
+</script>
+
+<style scoped></style>
