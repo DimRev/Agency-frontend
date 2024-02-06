b107cf6ac2ce1b18385984e1f1700c4a9823529d
DimRev <dimarev444@gmail.com>
2024-02-06 21:09:41 +0200

    Add ServicesSection

---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,19 @@
+<template >
+  <section class="flex flex-col mb-[150px]">
+    <SectionHeader >
+      <template v-slot:title>
+        <span class="text-gray-40">Our</span> Services
+      </template>
+      <template v-slot:content>
+        Our comprehensive range of services includes web design, mobile app development, SEO, social media marketing, and more. Whether you're a startup or an established enterprise, our experts will craft solutions that drive results.
+      </template>
+    </SectionHeader>
+  </section>
+</template>
+<script setup>
+import SectionHeader from './SectionHeader.vue'
+
+</script>
+<style >
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@
   <Hero class="col-start-1 col-end-4" />
   <section class="col-start-2">
     <ReasonsSection />
+    <ServicesSection />
   </section>
 </template>
 
 <script setup>
 import Hero from '../components/Hero.vue'
 import ReasonsSection from '../components/ReasonsSection.vue'
+import ServicesSection from '../components/ServicesSection.vue'
 </script>
 <style ></style>
