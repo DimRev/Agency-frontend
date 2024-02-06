7f8b82eedac8c2cd39533e4b5091d39b3b7246d3
DimRev <dimarev444@gmail.com>
2024-02-06 20:58:09 +0200

    Refactor add class-variance-authority pkg

--- src/components/ui/Link.vue
+++ src/components/ui/Link.vue
@@ -1,24 +1,27 @@
 <template>
-  <a v-if="type === 'ghost'" :href="href || '/'"
-    class="px-6 py-4 rounded-full transition-colors duration-300 hover:bg-gray-10 cursor-pointer">
-    <slot></slot>
-  </a>
-
-  <a v-else-if="type === 'full'" :href="href || '/'"
-    class="px-6 py-[18px] bg-gray-10 rounded-full text-white border transition-colors duration-300 border-gray-15 hover:bg-white hover:text-gray-10 hover:border-transparent cursor-pointer">
-    <slot></slot>
-  </a>
-
-  <a v-else-if="type === 'outline-w-img'" :href="href"
-    class="group/link pl-[34px] py-[18px] pr-[18px] border border-gray-12 rounded-full transition-colors duration-300 hover:bg-white hover:text-gray-10 hover:border-transparent cursor-pointer flex items-center gap-[30px]">
-    <slot></slot>
+  <a :href="href" :class="linkClasses({type})">
+    <slot/>
   </a>
 </template>
 
 <script setup>
+import { cva } from 'class-variance-authority'
+
+const linkClasses = cva(['duration-300','cursor-pointer','rounded-full','transition-colors'],{
+  variants:{
+    type: {
+      ghost: ['px-6', 'py-4', 'hover:bg-gray-10'],
+      full: ['px-6', 'py-[18px]', 'bg-gray-10', 'text-white', 'border', 'border-gray-15', 'hover:bg-white', 'hover:text-gray-10', 'hover:border-transparent'],
+      'outline-w-img': ['group/link pl-[34px]', 'py-[18px]', 'pr-[18px]', 'border border-gray-12', 'hover:bg-white', 'hover:text-gray-10', 'hover:border-transparent', 'flex', 'items-center', 'gap-[30px]']
+    }
+  }
+})
+
 const props = defineProps({
   href: String,
   type: String,
 })
+
+
 
 </script>
