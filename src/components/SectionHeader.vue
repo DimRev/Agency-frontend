c352112938fb4a7081a2af7b0fd7e0d6a4dbada4
DimRev <dimarev444@gmail.com>
2024-02-06 21:09:12 +0200

    Add cmp for section headers

---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="text-center">
+      <h1 class="mb-7 font-semibold text-[48px]">
+        <slot name="title" />
+      </h1>
+      <p class="text-gray-40">
+        <slot name="content" />
+      </p>
+    </header>
+</template>
