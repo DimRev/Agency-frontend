5907e26a1c93b1fac5a0c438772b12990f4ca74e
DimRev <dimarev444@gmail.com>
2024-02-06 22:40:40 +0200

    Fix color-transform of arrow pill

--- src/components/ui/IconPill.vue
+++ src/components/ui/IconPill.vue
@@ -1,6 +1,6 @@
 <template >
   <div v-if="type === 'arrow-right'"
-    class="group-hover/link:bg-white-80 bg-gray-10 px-5 py-[10px] border border-transparent group-hover/link:border-white-75 rounded-full transition-colors duration-300">
+    class="group-hover/link:bg-white-80 bg-gray-10 px-5 py-[10px] border border-transparent group-hover/link:border-white-75 rounded-full duration-100">
     <ArrowRightSvg />
   </div>
 </template>
