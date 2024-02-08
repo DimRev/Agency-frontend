1e3df57415d32603f57422d42a4d853181eb0073
DimRev <dimarev444@gmail.com>
2024-02-08 21:51:36 +0200

    Export FAQ item code to separate cmp

--- src/components/FAQList.vue
+++ src/components/FAQList.vue
@@ -1,20 +1,14 @@
 <template>
     <section class="grid grid-cols-2 gap-[50px]">
-      <div class="">
-        <article class="p-[34px]">
-          <div class="flex flex-col gap-[30px]">
-            <h3>How long does it take to complete a web development project?</h3>
-            <p class="text-gray-40">The timeline varies depending on the project's complexity and requirements. Our team strives to deliver projects on time while maintaining the highest quality standards.</p>
-          </div>
-          <div>
-          </div>
-        </article>
-      </div>
+      <FAQCollapsable />
+      <FAQCollapsable />
+      <FAQCollapsable />
+      <FAQCollapsable />
+      <FAQCollapsable />
+      <FAQCollapsable />
     </section>
 </template>
 
 <script setup>
-import HorDiv from './ui/HorDiv.vue';
-
-
+  import FAQCollapsable from './FAQCollapsable.vue'
 </script>
