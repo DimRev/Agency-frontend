1fd2a97464eab7f660d10631bacaba37d3397959
DimRev <dimarev444@gmail.com>
2024-02-08 21:50:25 +0200

    Add variant with no offset

--- src/components/ui/HorDiv.vue
+++ src/components/ui/HorDiv.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="relative w-full h-full">
-    <div class="top-[50%] absolute bg-gray-12 w-full h-[1px]"></div>
+    <div :class="linkClasses({offset})"></div>
   </div>
 </template>
+
+<script setup>
+import { cva } from 'class-variance-authority'
+
+const linkClasses = cva(['absolute', 'bg-gray-12', 'w-full' ,'h-[1px]'],{
+  variants: {
+    offset: {
+      '0': ['top-0'],
+      '50': ['top-[50%]'],
+    }
+  }
+})
+const props = defineProps({
+  offset: String,
+})
+</script>
