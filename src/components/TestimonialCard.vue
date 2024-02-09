40061b644605ad937420c057133212c31e640417
DimRev <dimarev444@gmail.com>
2024-02-09 12:00:59 +0200

    Refactor Cards using ui/Card and ui/...Frame

--- src/components/TestimonialCard.vue
+++ src/components/TestimonialCard.vue
@@ -1,13 +1,11 @@
 <template>
   <article>
-    <div class="bg-gray-20 wrapper-card">
-      <div class="modal-card-top-right-gradient flex flex-col gap-[30px] p-10 rounded-[20px] h-[303px]">
-        <div class="bg-gray-15-33 p-[14px] border border-gray-10 rounded-lg w-fit"><TwitterSvg /></div>
+    <Card type="testimonial">
+      <div class="bg-gray-15-33 p-[14px] border border-gray-10 rounded-lg w-fit"><TwitterSvg /></div>
         <p>
           <slot name="content" />
         </p>
-      </div>
-    </div>
+    </Card>
     <div class="translate-y-[-1px]">
       <CommentTringleSvg />
     </div>
@@ -22,8 +20,9 @@
 </template>
 
 <script setup>
-  import CommentTringleSvg from '../assets/svgs/CommentTringleSvg.vue'
-  import TwitterSvg from '../assets/svgs/TwitterSvg.vue'
+import CommentTringleSvg from '../assets/svgs/CommentTringleSvg.vue'
+import TwitterSvg from '../assets/svgs/TwitterSvg.vue'
+import Card from './ui/Card.vue'
 
   const props = defineProps({
     src: {type: String, required: true},
@@ -32,14 +31,6 @@
 </script>
 
 <style scoped>
-.modal-card-top-right-gradient {
-    background: linear-gradient(to top right, #0F0F0F 0%, #0F0F0F 55%, #1b1b1b 99%);
-  }
-.wrapper-card{
-  padding: 1px;
-  border-radius: 20px;
-}
-
 .bg-gray-15-33{
   background: #26262633;
 }
--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="wrapper-card">
-    <article class="modal-card-top-left-gradient flex flex-col gap-10 p-10 rounded-[20px]">
+  <Card type="work">
       <header class="">
         <div class="">
-          <div class="wrapper-img">
-            <div class="relative modal-img-right-bottom-gradient flex justify-center py-[118px] rounded-[20px]">
-              <div class="svg-wrapper">
-                <slot name="svg"/>
-              </div>
-              <Link href="/" type="outline" class="bottom-0 absolute translate-y-[50%]" img="with">View project details <ArrowRightSvg/></Link>
+          <Card type="work-svg">
+            <div class="svg-wrapper">
+              <slot name="svg"/>
             </div>
-          </div>
+            <Link href="/" type="outline" class="bottom-0 absolute translate-y-[50%]" img="with">View project details <ArrowRightSvg/></Link>
+          </Card>
         </div>
       </header>
       <main class="flex flex-col gap-6">
@@ -18,33 +15,15 @@
         <div class="flex justify-between"><h4><slot name="category" /></h4> <h4><slot name="date" /></h4></div>
         <p class="text-gray-40"><slot name="content" /></p>
       </main>
-    </article>
-  </div>
+    </Card>
 </template>
 
 <script setup>
-import ArrowRightSvg from '../assets/svgs/ArrowRightSvg.vue';
-import Link from './ui/Link.vue';
+import ArrowRightSvg from '../assets/svgs/ArrowRightSvg.vue'
+import Link from './ui/Link.vue'
+import Card from './ui/Card.vue'
 
 </script>
 
 <style scoped>
-  .modal-img-right-bottom-gradient {
-    background: linear-gradient(to right bottom, #0F0F0F 0%, #0F0F0F 55%, #1b1b1b 99%);
-  }
-  .wrapper-img{
-    padding: 1px;
-    background-color: #1a1a1a;
-    border-radius: 20px;
-  }
-
-  .modal-card-top-left-gradient {
-    background: linear-gradient(to top right, #0F0F0F 0%, #0F0F0F 55%, #1b1b1b 99%);
-  }
-
-  .wrapper-card{
-    padding: 1px;
-    background: linear-gradient(to top, transparent 0%, transparent 5% , #333333 99%);
-    border-radius: 20px;
-  }
 </style>
