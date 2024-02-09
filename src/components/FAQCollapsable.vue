d3f22628dc0e7496bfcd0142dc5ad8aa8b65fb4d
DimRev <dimarev444@gmail.com>
2024-02-09 12:01:58 +0200

    Add PlusSvg import for collapsible for later use

--- src/components/FAQCollapsable.vue
+++ src/components/FAQCollapsable.vue
@@ -6,7 +6,7 @@
             <p class="text-gray-40">The timeline varies depending on the project's complexity and requirements. Our team strives to deliver projects on time while maintaining the highest quality standards.</p>
           </div>
           <div class="flex items-center justify-center bg-gray-10 px-[16.67px] rounded-full h-[50px]">
-            <MinusSvg />
+            <MinusSvg/>
           </div>
         </div>
         <HorDiv offset="0" />
@@ -16,4 +16,5 @@
 <script setup>
 import HorDiv from './ui/HorDiv.vue'
 import MinusSvg from '../assets/svgs/MinusSvg.vue'
+import PlusSvg from '../assets/svgs/PlusSvg.vue'
 </script>
