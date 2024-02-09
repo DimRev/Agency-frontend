c7a66ae07c89b0cc2b73db467b70f87bf87624f5
DimRev <dimarev444@gmail.com>
2024-02-09 16:33:29 +0200

    Connect AppFooter to AppMain cmp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="grid grid-cols-[162px_1fr_162px] grid-rows-[auto_1fr] min-h-[100dvh]">
-    <Header class="col-span-3" />
+    <AppHeader class="col-span-3" />
     <Home />
+    <AppFooter class="col-span-3"/>
   </div>
 </template>
 
 <script setup>
-import Header from './components/Header.vue'
+import AppHeader from './components/Header.vue'
 import Home from './views/Home.vue'
-
+import AppFooter from './components/Footer.vue'
 </script>
 
 <style scoped>
