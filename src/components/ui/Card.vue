5b0c96537f107025745d9b38b47f062b08fa26a2
DimRev <dimarev444@gmail.com>
2024-02-09 16:31:32 +0200

    Add card variant for social networks

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -18,6 +18,7 @@
       'testimonial': ['card-wrapper-border-full', 'rounded-[20px]'],
       'partner': ['card-wrapper-border-fade-down', 'rounded-[20px]'],
       CTA: ['card-wrapper-border-fade-down', 'rounded-[20px]'],
+      social: ['card-wrapper-border-fade-down', 'rounded-full'],
     },
     modal: {
       service: ['card-modal-top-gradient', 'flex', 'flex-col', 'items-center', 'justify-between', 'gap-[50px]', 'px-20', 'pt-[120px]', 'pb-20', 'rounded-xl'],
@@ -26,6 +27,7 @@
       'testimonial':['card-modal-top-right-gradient', 'flex', 'flex-col', 'gap-[30px]', 'p-10', 'rounded-[20px]', 'h-[303px]'],
       'partner':['card-modal-top-right-gradient', 'rounded-[20px]', 'flex' ,'flex-col', 'gap-[60px]', 'px-10 pt-[100px] pb-20'],
       CTA: ['card-modal-top-gradient', 'rounded-[20px]', 'flex' ,'flex-col', 'gap-10', 'p-10'],
+      social: ['card-modal-top-gradient-lg', 'rounded-full', 'flex', 'p-5'],
     }
   }
 })
@@ -48,6 +50,9 @@ const props = defineProps({
 
   .card-modal-top-gradient{
     background: linear-gradient(to top, #0F0F0F 0%, #0F0F0F 85%, #1a1a1a 100%);
+  }
+  .card-modal-top-gradient-lg{
+    background: linear-gradient(to top, #0F0F0F 0%, #0F0F0F 55%, #1a1a1a 100%);
   }
   .card-modal-top-right-gradient {
     background: linear-gradient(to top right, #0F0F0F 0%, #0F0F0F 55%, #1b1b1b 99%);
