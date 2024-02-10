a9507b6eab25dac2f73486ef99f60ad451f598ef
DimRev <dimarev444@gmail.com>
2024-02-10 18:24:56 +0200

    Add responsiveness to ReasonCard

--- src/components/ReasonCard.vue
+++ src/components/ReasonCard.vue
@@ -1,12 +1,13 @@
 <template>
-  <article class="flex flex-col items-center justify-between gap-[50px] text-center">
+  <article class="flex flex-col items-center justify-between md:gap-10 max-md:gap-[30px] xl:gap-[50px] text-center">
     <div class="flex justify-center">
-      <div class="bg-gray-10 p-6 border-[10px] border-gray-30 rounded-full">
-        <slot name="svg"></slot>
+      <div class="relative top-left-gradient xl:p-6 max-xl:p-4 border-gray-30 xl:border-[10px] max-xl:border-[6px] rounded-full overflow-hidden svg-wrapper">
+        <slot name="svg" ></slot>
+        <img src="../assets/imgs/Abstract Design.png" alt="" class="abstract">
       </div>
     </div>
     <div>
-      <h3 class="mb-5 text-2xl">
+      <h3 class="mb-5 md:text-xl max-md:text-base xl:text-2xl">
         <slot name="header"></slot>
       </h3>
       <p class="text-gray-40">
@@ -15,7 +16,7 @@
     </div>
     <Link type="outline-w-img" :href="href">
     <span>Learn More</span>
-    <IconPill type="arrow-right" />
+    <IconPill type="arrow-right"  />
     </Link>
   </article>
 </template>
@@ -28,3 +29,18 @@ const props = defineProps({
   href: String,
 })
 </script>
+
+<style>
+.abstract{
+  position: absolute;
+  top: 0;
+  left: 0;
+  translate: 0 300%;
+  scale: 7;
+  z-index: 10;
+}
+
+.top-left-gradient {
+    background: linear-gradient(to top left, #0F0F0F 0%, #0F0F0F 55%, #333333 99%);
+  }
+</style>
