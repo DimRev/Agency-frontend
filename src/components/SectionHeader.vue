73feee873552eb6d832222c6cc895238463453bd
DimRev <dimarev444@gmail.com>
2024-02-10 13:48:28 +0200

    Add responsivity to section headers

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="text-center">
-      <h1 class="mb-7 font-semibold text-[48px]">
+      <h1 class="max-md:row-start-2 mb-3 md:mb-4 xl:mb-7 font-semibold text-[38px] md:text-5xl max-md:text-center xl:text-[58px]">
         <slot name="title" />
       </h1>
       <p class="text-gray-40">
